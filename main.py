bad6eef995a8f487bd2b95659e9420d8166052fc
root <root@CDR-WAREHOUSE.(none)>
2013-10-09 11:30:33 +0200

    add configuration file utility

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import subprocess
-
+import ConfigParser
 
 class my_os:
+	
+	
 	os_name="unknown"
 	
 	def __init__(self):
 		print "what is my  computer"
+		config = ConfigParser.RawConfigParser()
+                config.read('config.cfg')
+                verbosity = config.get("general","log-level")
+		print verbosity
 	def whoami(self):
 		print "who ami"
-		subprocess.call(["whoami"])
+		me = subprocess.check_output(["whoami"])
+		print me
 	def osinfo(self):
 		print "os info"
-		subprocess.call(["cat","/etc/issue"])
+		subprocess.call(["cat", "/etc/issue"])
+		subprocess.call(["uname", "-a"])
 	def network_info(self):# not working
 		print "info on "
 		subprocess.call(["ping", "-b", "3", "8.8.8.8"])	
