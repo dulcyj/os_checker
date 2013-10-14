fec8ca6979ae9b7b3bd25c9364331eeb27233f81
root <root@CDR-WAREHOUSE.(none)>
2013-10-14 16:55:28 +0200

    refactor, and renice XML output

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import subprocess
 import ConfigParser
 import xml.etree.ElementTree as ET
+from xml.dom import minidom
 
 class my_os:
 	
@@ -13,14 +14,18 @@ class my_os:
 		config = ConfigParser.RawConfigParser()
                 config.read('config.cfg')
                 verbosity = config.get("general","log-level")
-		print verbosity
-		b = ET.SubElement(self.xml_root, 'b')
-		ET.dump(self.xml_root)
-		ET.ElementTree(self.xml_root).write("output.xml")
+		general_xml = ET.SubElement(self.xml_root, "general")
+		verbosity_xml = ET.SubElement(general_xml, "verbosity")
+		verbosity_xml.text = verbosity 	
+		print verbosity 	
 	def whoami(self):
 		print "who ami"
 		me = subprocess.check_output(["whoami"])
-		print me
+		group = subprocess.check_output(["groups"])
+		user = ET.SubElement(self.xml_root, "user")
+		user.text = me.rstrip('\n')
+		group_xml = ET.SubElement(self.xml_root,"group")
+		group_xml.text = group.rstrip('\n')
 	def osinfo(self):
 		print "os info"
 		#os1 = subprocess.check_output(["cat /etc/issue"])
@@ -30,11 +35,18 @@ class my_os:
 		print "info on "
 		subprocess.call(["ping", "-b", "3", "8.8.8.8"])	
 	def test_network(self):
-		print "start"	
+		print "start"
+	def export(self):
+		rough_string = ET.tostring(self.xml_root, 'utf-8')
+		reparsed = minidom.parseString(rough_string)
+		print reparsed.toprettyxml(indent="  ")
+		ET.dump(self.xml_root)
+		ET.ElementTree(self.xml_root).write("output.xml")
 
 
 if __name__ == '__main__':
 	me = my_os()
 	me.whoami()
 	me.osinfo()
-	#me.network_info()
+	me.network_info()
+	me.export()
