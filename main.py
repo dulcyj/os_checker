13b826c14d1ca2f19a2a7267a7d4c526819b2359
root <root@CDR-WAREHOUSE.(none)>
2013-10-14 13:52:00 +0200

    using subprocess os.system

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import subprocess
 import ConfigParser
+import xml.etree.ElementTree as ET
 
 class my_os:
 	
@@ -12,14 +13,21 @@ class my_os:
                 config.read('config.cfg')
                 verbosity = config.get("general","log-level")
 		print verbosity
+		a = ET.Element('a')
+		b = ET.SubElement(a, 'b')
+		c = ET.SubElement(a, 'c')
+		d = ET.SubElement(c, 'd')
+		ET.dump(a)
+		#a.write("output.xml")	
 	def whoami(self):
 		print "who ami"
 		me = subprocess.check_output(["whoami"])
 		print me
 	def osinfo(self):
 		print "os info"
-		subprocess.call(["cat", "/etc/issue"])
-		subprocess.call(["uname", "-a"])
+		os1 = subprocess.check_output(["cat /etc/issue"])
+		os2 = subprocess.check_ouput(["uname -a"])
+		print os1 + os2 
 	def network_info(self):# not working
 		print "info on "
 		subprocess.call(["ping", "-b", "3", "8.8.8.8"])	
