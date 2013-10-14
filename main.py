34feafc177c1d17e826339aa21c5123a7fe17e8b
root <root@CDR-WAREHOUSE.(none)>
2013-10-14 14:00:33 +0200

    add XML feature with output

--- main.py
+++ main.py
@@ -5,29 +5,27 @@ import xml.etree.ElementTree as ET
 class my_os:
 	
 	
-	os_name="unknown"
-	
+	os_name = "unknown"
+	xml_root = ET.Element('root')
+ 	
 	def __init__(self):
 		print "what is my  computer"
 		config = ConfigParser.RawConfigParser()
                 config.read('config.cfg')
                 verbosity = config.get("general","log-level")
 		print verbosity
-		a = ET.Element('a')
-		b = ET.SubElement(a, 'b')
-		c = ET.SubElement(a, 'c')
-		d = ET.SubElement(c, 'd')
-		ET.dump(a)
-		#a.write("output.xml")	
+		b = ET.SubElement(self.xml_root, 'b')
+		ET.dump(self.xml_root)
+		ET.ElementTree(self.xml_root).write("output.xml")
 	def whoami(self):
 		print "who ami"
 		me = subprocess.check_output(["whoami"])
 		print me
 	def osinfo(self):
 		print "os info"
-		os1 = subprocess.check_output(["cat /etc/issue"])
-		os2 = subprocess.check_ouput(["uname -a"])
-		print os1 + os2 
+		#os1 = subprocess.check_output(["cat /etc/issue"])
+		#os2 = subprocess.check_ouput(["uname -a"])
+		#print os1 + os2 
 	def network_info(self):# not working
 		print "info on "
 		subprocess.call(["ping", "-b", "3", "8.8.8.8"])	
